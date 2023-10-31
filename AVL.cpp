779aca31281f15f964b2977c808a561340b0c2c7
Lidia Messias <lidia.mess@gmail.com>
2023-10-31 12:28:11 -0300

    Implementação das funções girarEsquerda() e girarDireita()

--- AVL.cpp
+++ AVL.cpp
@@ -207,12 +207,26 @@ NO* insereArvore(NO* no, int valor)
 NO* girarDireita(NO* no)
 {
 	// sua implementação vai aqui
+
+	NO* novaraiz = no->esq;
+	NO* aux = novaraiz->dir;
+	novaraiz->dir = no;
+	no->esq = aux; 
+
+	no = novaraiz;
 	return no;
 }
 
 NO* girarEsquerda(NO* no)
 {
 	// sua implementação vai aqui
+	
+	NO* novaraiz = no->dir;
+	NO* aux = novaraiz->esq;
+	novaraiz->esq = no;
+	no->dir = aux; 
+
+	no = novaraiz;
 	return no;
 }
 
